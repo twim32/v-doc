64b02519d1be50e195ac87a82800921bc176954e
twim32 <twim32@mail.ru>
2022-07-06 19:30:41 +0700

    Resolve probles with rows and table sync

--- src/components/Document/ProductRow.vue
+++ src/components/Document/ProductRow.vue
@@ -1,31 +1,84 @@
 <template>
-  <tr>
-    <td>{{ id }}</td>
-    <td>{{ name }}</td>
-    <td><input type="number" name="quantity" :value="quantity"></td>
-    <td><input type="number" name="price" :value="price"></td>
-    <td><input type="number" name="discount" :value="discount"></td>
-    <td><input type="number" name="amount" :value="amount"></td>
+  <tr style="user-select:none;">
+    <td>{{ data.id }}</td>
+    <td>{{ data.name }}</td>
+    <td>
+      <input type="number" v-if="is_editable" name="quantity" v-model="data.quantity" @change="calcPrice" min="0.01">
+      <span v-else>{{data.quantity}}</span>
+    </td>
+    <td>
+      <input type="number" v-if="is_editable" name="original_price" v-model="data.original_price" readonly >
+      <span v-else>{{data.original_price}}</span>
+    </td>
+    <td>
+      <input type="number" v-if="is_editable" name="price" v-model="data.price" @change="calcDiscount">
+      <span v-else>{{data.price}}</span>
+    </td>
+    <td>
+      <input type="number" v-if="is_editable" name="discount" v-model="data.discount" @change="calcPrice" min="0" max="100">
+      <span v-else>{{data.discount}}</span>
+    </td>
+    <td>
+      <input type="number" v-if="is_editable" name="amount" v-model="data.amount" readonly >
+      <span v-else>{{data.amount}}</span>
+    </td>
+    <td>
+      <button @click="toggle"><span v-if="is_editable">Save</span><span v-else>Edit</span></button>
+      <button @click="remove">Remove</button>
+    </td>
   </tr>
 </template>
 
 <script>
 export default {
   name: "ProductRow",
-  props: ["id", 'name', 'quantity', 'price', 'discount'],
-
+    // props: ['data'],
+    props: ['data'],
+    data: function() {
+      return {
+        is_editable: false,
+      }
+    },
+    
   created: function () {
-    console.log('created');
+    this.calcAmount()
+    console.log(this);
   },
   methods: {
+    onChange : function() {
+      this.calcAmount();
+
+      // this.$emit('updated:amount');
+    },
+    calcPrice: function() {
+      this.data.price = parseFloat((this.data.original_price / 100 * this.data.discount).toFixed(2),2);
+      this.calcAmount();
+    },
+    calcAmount: function() {
+      this.data.amount = parseFloat((this.data.quantity * this.data.price).toFixed(2),2);
+      this.$emit('item__changed');
+    },
+    calcDiscount: function() {
+      this.data.discount = parseFloat((this.data.price / this.data.original_price * 100).toFixed(2),2);
+      this.calcAmount();
+    },
+
+    toggle: function() {
+      this.is_editable = !this.is_editable;
+    },
+    remove: function() {
+      console.log('removed');
+      this.$emit('item__removed', this.data);
+    }
   },
   watch: {
   },
   computed: {
-    amount: function () {
-      return (this.quantity * this.price * this.discount);
-    },
-
+    // _price: {
+    //   set: function(value) {
+    //     this.data.price = 333 * value
+    //   }
+    // },
   },
 
 };
--- src/components/Document/ProductTable.vue
+++ src/components/Document/ProductTable.vue
@@ -6,21 +6,21 @@
           <td>id</td>
           <td>name</td>
           <td>quantity</td>
+          <td>original_price</td>
           <td>price</td>
           <td>discount</td>
-          <td>total</td>
+          <td>amount</td>
+          <td></td>
         </tr>
       </thead>
       <tbody>
-        <ProductRow
-            v-for="row in items"
-            v-bind.sync="row"
-            v-bind:key="row.id"
-            ref="products"
-        ></ProductRow>
+        
+         <ProductRow @item__changed="calcTotal" @item__removed="remove(index)" v-for="(data, index) in items" :data="data" :key="index"
+         ></ProductRow>
       </tbody>
       <tfoot>
       <tr>
+        <td></td>
         <td></td>
         <td></td>
         <td></td>
@@ -29,6 +29,8 @@
           <button type="submit" v-on:click="calcTotal">test</button>
         </td>
         <td>{{ total }}</td>
+        <td></td>
+
       </tr>
       </tfoot>
     </table>
@@ -43,12 +45,19 @@ export default {
   data: function () {
     return {
       items: [
-        { id: 1, name: "a", quantity: 3, price: 4, discount: 0.1, amount: 0 },
-        { id: 2, name: "b", quantity: 3, price: 5, discount: 0.1, amount: 0 },
-        { id: 3, name: "c", quantity: 3, price: 6, discount: 0.1, amount: 0 },
-        { id: 4, name: "d", quantity: 3, price: 7, discount: 0.1, amount: 0 },
-        { id: 5, name: "e", quantity: 3, price: 8, discount: 0.1, amount: 0 },
-        { id: 6, name: "f", quantity: 3, price: 9, discount: 0.1, amount: 0 },
+        { id: 1, name: "a", quantity: 1, original_price: 40, price: 40, discount: 10, amount: 0 },
+        { id: 2, name: "b", quantity: 1, original_price: 50, price: 50, discount: 10, amount: 0 },
+        { id: 3, name: "c", quantity: 1, original_price: 60, price: 60, discount: 10, amount: 0 },
+        { id: 4, name: "d", quantity: 40, original_price: 70, price: 70, discount: 10, amount: 0 },
+        { id: 5, name: "e", quantity: 50, original_price: 80, price: 80, discount: 10, amount: 0 },
+        { id: 6, name: "f", quantity: 60, original_price: 90, price: 90, discount: 10, amount: 0 },
+
+        // { id: 1, name: "a", quantity: 3, price: 4, discount: 0.1},
+        // { id: 2, name: "b", quantity: 3, price: 5, discount: 0.1},
+        // { id: 3, name: "c", quantity: 3, price: 6, discount: 0.1},
+        // { id: 4, name: "d", quantity: 3, price: 7, discount: 0.1},
+        // { id: 5, name: "e", quantity: 3, price: 8, discount: 0.1},
+        // { id: 6, name: "f", quantity: 3, price: 9, discount: 0.1},
       ],
       total: 0,
     };
@@ -59,12 +68,18 @@ export default {
 
   methods: {
     calcTotal: function() {
+      console.log('calculating total', this.items);
       let total = 0;
-      this.$refs.products.forEach(product => {
+      this.items.forEach(product => {
         total += product.amount;
       })
       this.total = total;
-    }
+      // this.total = 333;
+    },
+    remove: function(index) {
+      this.items.splice(index, 1);
+      this.calcTotal();
+    },
   },
 
   mounted() {
